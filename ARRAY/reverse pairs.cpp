97678b7416dd5c2d2b770ccf44f96522b33c7893
Sarthak Shukla <sarthakshukla500@gmail.com>
2024-01-20 20:37:43 +0530

    10th Day2

--- ARRAY/reverse pairs.cpp
+++ ARRAY/reverse pairs.cpp
@@ -59,12 +59,24 @@ int mergesort(vector<int> & arr,int low,int high){
     return cnt;
 }
 
+
+
 int team(vector <int> & skill, int n)
 {
     // Write your code here.
     return mergesort(skill,0,n-1);
 }
+int countPairsbrute(vector<int>&a, int n) {
 
+    // Count the number of pairs:
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] > 2 * a[j]) cnt++;
+        }
+    }
+    return cnt;
+}
 
 int main(){
 

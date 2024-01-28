aa054be14d8fb8199782cd4c1f6687cf4110e133
Sarthak Shukla <sarthakshukla500@gmail.com>
2024-01-28 22:57:44 +0530

    17th Day

---
+++ BINARY SEARCH/Find the Smallest Divisor Given a Threshold.cpp
@@ -0,0 +1,38 @@
+//Find the Smallest Divisor Given a Threshold
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+int sumof(vector<int>& arr, int d){
+	int n=arr.size();int sum=0;
+	for(int i=0;i<n;i++){
+		sum+=ceil((double)(arr[i])/(double)(d));
+	}
+	return sum;
+}
+    int smallestDivisor(vector<int>& arr, int limit) {
+    int n=arr.size();int maxi=INT_MIN;
+	for(int i=0;i<n;i++){
+		maxi=max(maxi,arr[i]);
+	}
+	int low=1,high=maxi;
+	if(limit<n) return -1;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(sumof(arr,mid)<=limit){
+			high=mid-1;
+		} else low=mid+1;
+
+	}
+	return low;
+    }
+};
+
+int main(){
+
+    
+
+    return 0;
+}

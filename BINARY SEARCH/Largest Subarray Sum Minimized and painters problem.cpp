1bdcaf2a8bcb5484ec1fbe805d6e9698a05e95ed
Sarthak Shukla <sarthakshukla500@gmail.com>
2024-01-31 08:37:46 +0530

    19th Day

---
+++ BINARY SEARCH/Largest Subarray Sum Minimized and painters problem.cpp
@@ -0,0 +1,34 @@
+//Largest Subarray Sum Minimized and painters problem
+#include<bits/stdc++.h>
+
+int students(vector<int>& arr, int m){
+    long long last=0;int stu=1;
+    for(int i=0;i<arr.size();i++){
+        if(last+arr[i]<=m){
+            last+=arr[i];
+            
+        } else {
+            stu++;
+            last=arr[i];
+        }
+    }
+    return stu;
+
+}
+int findPages(vector<int>& arr, int n, int m) {
+    // Write your code here.
+    if(n<m) return -1;
+    int low=*max_element(arr.begin(),arr.end());
+    int high=accumulate(arr.begin(),arr.end(),0);
+    while(low<=high){
+        int mid=(low+high)/2,noofstudents=students(arr,mid);
+        if(noofstudents>m) low=mid+1;
+        else high=mid-1;
+
+    }
+    return low;
+}
+int largestSubarraySumMinimized(vector<int> a, int k) {
+    // Write Your Code Here
+    return findPages(a,a.size(),k);
+}

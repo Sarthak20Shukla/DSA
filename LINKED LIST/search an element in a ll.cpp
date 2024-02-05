43a1937f1b26af2b00123dc24e6adce9eabb9aaf
Sarthak Shukla <sarthakshukla500@gmail.com>
2024-02-05 21:56:36 +0530

    22nd Day

---
+++ LINKED LIST/search an element in a ll.cpp
@@ -0,0 +1,53 @@
+//search an element in a ll
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+    template <typename T>
+    class Node
+    {
+    public:
+        T data;
+        Node<T> *next;
+        Node()
+        {
+            this->data = 0;
+            this->next = NULL;
+        }
+        Node(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+        Node(T data, T* next)
+        {
+            this->data = data;
+            this->next = next;
+        }
+    };
+
+*****************************************************************/
+
+int searchInLinkedList(Node<int> *head, int k) {
+    // Write your code here.
+    Node<int>* temp=head;
+    while(temp!=NULL){
+        if(temp->data==k){
+            return 1;
+            break;
+        }
+        temp=temp->next;
+    }
+    return 0;
+}
+
+int main(){
+
+    
+
+    return 0;
+}

edf31c9d35acdf581ae3d65f32174bfddc9cff7d
Sarthak Shukla <sarthakshukla500@gmail.com>
2024-02-16 22:39:34 +0530

    31st Day

---
+++ TREE/calc the no of nodes in a level.cpp
@@ -0,0 +1,18 @@
+//calc the no of nodes in a level
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#include<bits/stdc++.h>
+int numberOfNodes(int N){
+    // Write your code here.
+    return pow(2,N-1);
+}
+
+int main(){
+
+    
+
+    return 0;
+}
+

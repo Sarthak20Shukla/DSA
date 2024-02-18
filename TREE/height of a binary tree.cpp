d9207e6291e0e5126b1fa1717dd950b6c2d3bbee
Sarthak Shukla <sarthakshukla500@gmail.com>
2024-02-18 09:12:02 +0530

    33rd Day

---
+++ TREE/height of a binary tree.cpp
@@ -0,0 +1,44 @@
+//height of a binary tree
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode
+    {
+    public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val)
+        {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+int heightOfBinaryTree(TreeNode<int> *root)
+{
+	// Write your code here.
+    if(root==NULL) return 0;
+    int l=heightOfBinaryTree(root->left);
+    int r=heightOfBinaryTree(root->right);
+
+    return 1+max(l,r);
+}
+
+
+int main(){
+
+    
+
+    return 0;
+}

98eeaff661e85ec2e16253bfb4a890a0472f0b66
Sarthak Shukla <sarthakshukla500@gmail.com>
2024-02-20 17:47:28 +0530

    35th Day

---
+++ TREE/bottom view of a binary tree.cpp
@@ -0,0 +1,54 @@
+//bottom view of a binary tree
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+vector<int> bottomView(TreeNode<int> * root){
+    // Write your code here.
+    vector<int> ans;
+    if(root==NULL) return ans;
+    queue<pair<TreeNode<int> *,int>> q;
+    map<int,int> mpp;
+    q.push({root,0});
+    while(!q.empty()){
+        auto it=q.front();
+        q.pop();
+        TreeNode<int> *node=it.first;
+        int line=it.second;
+        mpp[line]=node->data;
+        if(node->left!=NULL) q.push({node->left,line-1});
+        if(node->right!=NULL) q.push({node->right,line+1});
+    }
+    for(auto it:mpp){
+        ans.push_back(it.second);
+    }
+    return ans;
+}
+
+
+int main(){
+
+    
+
+    return 0;
+}

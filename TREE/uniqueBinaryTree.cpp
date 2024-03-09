ecc3039f22febc71db74543fd8be7c9ad9a827c2
Sarthak Shukla <sarthakshukla500@gmail.com>
2024-03-09 19:26:15 +0530

    39th Day

---
+++ TREE/uniqueBinaryTree.cpp
@@ -0,0 +1,19 @@
+//unique binary trees
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int uniqueBinaryTree(int a, int b){
+    // Write your code here.
+    if(a==2&&b==2) return false;
+    if(a==2||b==2) return true;
+
+    return false;
+}
+
+int main(){
+
+    
+
+    return 0;
+}

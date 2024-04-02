78ba369a193d76068f64a7e58fd04fc9cd5d969e
Sarthak Shukla <sarthakshukla500@gmail.com>
2024-04-02 22:32:59 +0530

    44th Day

---
+++ TREE/kthSmallestor largest.cpp
@@ -0,0 +1,41 @@
+//kthSmallestor largest
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root,int k,int &ans,int &cnt){
+        if(root==NULL) return;
+        inorder(root->left,k,ans,cnt);
+        cnt++;
+        if(cnt==k){
+            ans=root->val;
+            return;
+        }
+        inorder(root->right,k,ans,cnt);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        int ans=-1,cnt=0;
+        inorder(root,k,ans,cnt);
+        return ans;
+    }
+};
+
+int main(){
+
+    
+
+    return 0;
+}

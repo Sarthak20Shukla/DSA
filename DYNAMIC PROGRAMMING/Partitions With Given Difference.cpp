59468dffd602a8c46133d1b258f27c2155e02761
Sarthak Shukla <sarthakshukla500@gmail.com>
2024-06-01 06:59:11 +0530

    72nd Day

---
+++ DYNAMIC PROGRAMMING/Partitions With Given Difference.cpp
@@ -0,0 +1,46 @@
+// Partitions With Given Difference
+//tabulation
+#include <bits/stdc++.h> 
+int mod=(int)(1e9+7);
+int findWays(vector<int>& arr, int k)
+{
+	// Write your code here.
+	int n=arr.size();
+    vector<vector<int>> dp(n,vector<int>(k+1,0));
+	if(arr[0] == 0) dp[0][0] =2;  // 2 cases -pick and not pick
+    else dp[0][0] = 1;  // 1 case - not pick
+    
+    if(arr[0]!=0 && arr[0]<=k) dp[0][arr[0]] = 1; 
+
+    for(int i = 1; i < n; i++){
+
+        for(int sum = 0; sum <= k; sum ++){
+
+            int not_take = dp[i-1][sum];
+
+            int take = 0;
+
+            if(arr[i] <= sum) take = dp[i-1][sum-arr[i]];
+
+            dp[i][sum] = (take + not_take)%mod;
+
+        }
+
+    }
+
+    return dp[n-1][k];
+	
+}
+int countPartitions(int n, int d, vector<int> &arr) {
+    // Write your code here.
+    int totsum=0;
+    for(int i=0;i<n;i++){
+        totsum+=arr[i];
+    }
+    if(totsum-d<0 || (totsum-d)%2!=0){
+        return 0;
+    }
+    return findWays(arr,(totsum-d)/2);
+}
+
+

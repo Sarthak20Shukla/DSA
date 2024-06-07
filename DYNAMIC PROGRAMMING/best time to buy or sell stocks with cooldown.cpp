3e340f414ac83dd43816b367241df8f0347475cd
Sarthak Shukla <sarthakshukla500@gmail.com>
2024-06-08 04:58:32 +0530

    77th Day

--- DYNAMIC PROGRAMMING/best time to buy or sell stocks with cooldown.cpp
+++ DYNAMIC PROGRAMMING/best time to buy or sell stocks with cooldown.cpp
@@ -3,7 +3,7 @@
 //memoization
 int f(int ind,int buy,vector<int>& prices,vector<vector<int>> &dp){
     int n=prices.size();
-    if(ind==n) return 0;
+    if(ind>=n) return 0;
     if(dp[ind][buy]!=-1) return dp[ind][buy];
     int profit=0;
     if(buy){

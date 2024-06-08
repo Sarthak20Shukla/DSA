8469a3f286ac66e2a9d2b0f3203651c6a74bd040
Sarthak Shukla <sarthakshukla500@gmail.com>
2024-06-09 00:25:42 +0530

    68th Day

---
+++ DYNAMIC PROGRAMMING/print lis.cpp
@@ -0,0 +1,25 @@
+//print lis
+ vector<int> dp(n,1) , hash(n);
+        int maxi = 1 , last = 0;
+        for(int i=0;i<n;i++){
+            hash[i]=i;
+            for(int prev = 0;prev<i;prev++){
+                if(nums[i]>nums[prev] && 1+dp[prev] > dp[i]){
+                    dp[i] = 1 + dp[prev];
+                    hash[i]=prev;
+                }
+            }
+            if(dp[i]>maxi){
+                maxi = dp[i];
+                last = i;
+            }
+        }
+        
+        vector<int> ans;
+        ans.push_back(nums[last]);
+        while(last != hash[last]){
+            last = hash[last];
+            ans.push_back(nums[last]);
+        }
+        reverse(ans.begin() , ans.end());
+        return ans;

5b27503e8abd20c3d1f340642442e3adcfb88ad3
Sarthak Shukla <sarthakshukla500@gmail.com>
2024-08-15 19:49:38 +0530

    leetcode potd

---
+++ cp/Leetcode/lemonade change.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five=0,tens=0;
+        for(int i=0;i<bills.size();i++){
+            if(bills[i]==5){
+                five+=1;
+
+            } else if(bills[i]==10){
+                if(five){
+                    five=five-1;
+                    tens+=1;
+                } else {
+                    return false;
+                }
+            }
+
+            else {
+                if(five && tens){
+                    tens-=1;
+                    five-=1;
+                }
+                else if(five>=3){
+                    five-=3;
+          
+                }
+                else return false;
+            }
+        }
+        return true;
+        
+    }
+};
